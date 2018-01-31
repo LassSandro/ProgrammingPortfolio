a38a57c1c7babd1031aca6ed25b65cc94d96774b
Sandro Maglione <lass.maglio@gmail.com>
2018-01-31 09:10:43 +0100

    Change initial

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+  <head>
+    <title>Main Page</title>
+  </head>
+  <body>
+
+    <table>
+      <tr>
+        <td>
+          
+        </td>
+      </tr>
+    </table>
+  </body>
+</html>
