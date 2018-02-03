557a913785a23d9884ebf6635da0b0fa1b73ac32
Sandro Maglione <lass.maglio@gmail.com>
2018-02-03 19:21:36 +0100

    Version 1.0.0 is now available

---
+++ config.php
@@ -0,0 +1,148 @@
+<?php
+/* User image (filename.png/jpg/jpeg...) */
+$myImage = "default.jpeg";
+
+/* Name */
+$myName = "James Smith";
+
+/* Profession */
+$myProfession = "Developer";
+
+/* My description */
+$myBio = "I am a developer. I love cats, computers and visit new places and meeting new people";
+
+/* Contact me email */
+$contactMail = "lass.maglio@gmail.com";
+
+
+/* Social links */
+$myLinks = array(
+  'email' => 'mailto:lass.maglio@gmail.com',
+  'github' => 'https://github.com/LassSandro',
+  'linkedin' => 'https://www.linkedin.com/in/sandro-maglione97/',
+  'website' => 'http://www.cmprogrammers.com/',
+  'twitter' => 'https://twitter.com/SandroMaglione',
+  'playstore' => 'https://play.google.com/store/apps/dev?id=7196118552342692066',
+  'facebook' => 'https://www.facebook.com/sandro.maglione.35'
+);
+
+/* Display text */
+$myLinksText = array(
+  'email' => 'lass.maglio@gmail.com',
+  'github' => 'LassSandro',
+  'linkedin' => 'Sandro Maglione',
+  'website' => 'cmprogrammers.com',
+  'twitter' => '@SandroMaglione',
+  'playstore' => 'C.M.Programmers',
+  'facebook' => 'Sandro Maglione'
+);
+
+/* Icons (Default) */
+$myLinkIcons = array(
+  'email' => 'fa fa-envelope',
+  'github' => 'fab fa-github',
+  'linkedin' => 'fab fa-linkedin-in',
+  'website' => 'fa fa-globe',
+  'twitter' => 'fab fa-twitter',
+  'playstore' => 'fab fa-google-play',
+  'facebook' => 'fab fa-facebook-f'
+);
+
+
+/*
+1 = Known
+2 = Learning
+0 = Not Known
+*/
+$known = array(
+  'html5-plain-wordmark' => 0,
+  'css3-plain-wordmark' => 0,
+  'sass-original' => 2,
+  'php-plain' => 1,
+  'javascript-plain' => 0,
+  'jquery-plain-wordmark' => 0,
+  'csharp-line' => 1,
+  'cplusplus-line' => 0,
+  'wordpress-plain' => 0,
+  'bootstrap-plain' => 0,
+  'mysql-plain-wordmark' => 0,
+  'photoshop-plain' => 0,
+  'illustrator-plain' => 0,
+  'apache-line-wordmark' => 2,
+  'angularjs-plain' => 1,
+  'c-line' => 0,
+  'coffeescript-original' => 0,
+  'd3js-plain' => 0,
+  'django-line' => 1,
+  'go-plain' => 0,
+  'gradle-plain' => 0,
+  'laravel-plain' => 1,
+  'less-plain-wordmark' => 0,
+  'linux-plain' => 0,
+  'mongodb-plain-wordmark' => 0,
+  'nodejs-plain' => 0,
+  'python-plain' => 2,
+  'rails-plain' => 2,
+  'react-original' => 0,
+  'ruby-plain' => 1,
+  'swift-plain' => 0,
+  'symfony-original' => 0,
+  'typescript-plain' => 0,
+  'vuejs-plain-wordmark' => 0
+);
+
+/* Write the tooltip description of the ones you choose */
+/* Or the percentage in case you are learning it */
+$knownDescription = array(
+  'html5-plain-wordmark' => 0,
+  'css3-plain-wordmark' => 0,
+  'sass-original' => "83%",
+  'php-plain' => "6 years experince",
+  'javascript-plain' => 0,
+  'jquery-plain-wordmark' => 0,
+  'csharp-line' => "I like C#",
+  'cplusplus-line' => 0,
+  'wordpress-plain' => 0,
+  'bootstrap-plain' => 0,
+  'mysql-plain-wordmark' => 0,
+  'photoshop-plain' => 0,
+  'illustrator-plain' => 0,
+  'apache-line-wordmark' => "45%",
+  'angularjs-plain' => "Favourite framework",
+  'c-line' => 0,
+  'coffeescript-original' => 0,
+  'd3js-plain' => 0,
+  'django-line' => "New in my list",
+  'go-plain' => 0,
+  'gradle-plain' => 0,
+  'laravel-plain' => "Back-end programming",
+  'less-plain-wordmark' => 0,
+  'linux-plain' => 0,
+  'mongodb-plain-wordmark' => 0,
+  'nodejs-plain' => 0,
+  'python-plain' => "21%",
+  'rails-plain' => "3%",
+  'react-original' => 0,
+  'ruby-plain' => "Always a good choice",
+  'swift-plain' => 0,
+  'symfony-original' => 0,
+  'typescript-plain' => 0,
+  'vuejs-plain-wordmark' => 0
+);
+
+
+/*
+Key: Skill name
+Value: Skill description
+*/
+$skills = array(
+  'Project Management' =>
+  'Explain your skill and your experiences. Why would someone consider it?',
+  'Public Speaking' =>
+  'Explain your skill and your experiences. Why would someone consider it?',
+  'Public Relations' =>
+  'Explain your skill and your experiences. Why would someone consider it?',
+  'Buisness Management' =>
+  'Explain your skill and your experiences. Why would someone consider it?'
+);
+?>
---
+++ index.php
@@ -0,0 +1,198 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <!-- Font Awesome -->
+    <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+
+    <!-- Devicon icons (http://konpa.github.io/devicon/) -->
+    <link rel="stylesheet" href="https://cdn.rawgit.com/konpa/devicon/df6431e323547add1b4cf45992913f15286456d3/devicon.min.css">
+
+    <!-- Custom style -->
+    <link rel="stylesheet" href="./style/css/style.css" />
+
+    <title>WhatICode</title>
+  </head>
+  <body>
+    <?php include("config.php"); ?>
+
+    <!-- All page container -->
+    <div class="container-fluid">
+
+
+      <!-- Content row -->
+      <div class="row">
+
+
+        <!-- Left content informations -->
+        <div class="col-12 col-sm-5 col-md-5 col-lg-3 pb-5 pt-4 pl-4 pr-4 my-bg-primary">
+
+          <!-- User image -->
+          <div class="text-center">
+            <img src="./images/<?php echo $myImage; ?>" class="img-fluid" alt="User image">
+          </div>
+
+          <!-- Main info container -->
+          <!-- class="info-sidebar" to change the text size Responsive -->
+          <div class="info-sidebar my-sidebar-padding mb-2 mt-1">
+            <!-- Name -->
+            <h1 class="my-text-default pt-2"><?php echo $myName; ?></h1>
+
+            <!-- Profession -->
+            <h2 class="my-text-subtext"><?php echo $myProfession; ?></h2>
+          </div>
+
+          <!-- Bio (card) -->
+          <div class="card my-bg-primary my-card-bio my-sidebar-padding mb-5">
+            <div class="card-body">
+              <p class="card-text my-text-subtext"><?php echo $myBio; ?></p>
+            </div>
+          </div>
+
+          <!-- Contact links -->
+          <div class="list-group list-group-flush mt-3">
+            <?php
+              foreach($myLinks as $data => $value) {
+            ?>
+              <a target="_blank" href="<?php echo $value; ?>" class="list-group-item list-group-item-action"><i class="<?php echo $myLinkIcons[$data]; ?> mr-3"></i><span class="my-text-list"><?php echo $myLinksText[$data]; ?></span></a>
+            <?php } ?>
+          </div>
+
+          <!-- Contact me button -->
+          <a href="mailto:<?php echo $contactMail; ?>"class="btn btn-primary my-bg-btn btn-block mt-5 p-2 font-weight-bold">CONTACT ME</a>
+        </div>
+
+
+
+
+        <!-- Right content section -->
+        <div class="col-12 col-sm-7 col-md-7 col-lg-9 my-bg-secondary pt-5 pr-5 pl-5 pb-3">
+
+          <!-- Section 1 - Known -->
+          <div class="container mb-5">
+            <h4 class="my-text-title mb-0">Known Technologies</h4>
+            <p class="my-text-title small"><i>Programming languages, frameworks, software and other <small>(Click to see more information)</small></i></p>
+
+            <!-- Hr -->
+            <hr class="my-hr-style mb-3"/>
+
+            <!-- Data list -->
+            <div class="my-bg-secondary">
+              <?php
+                foreach($known as $data => $value) {
+                  if ($value == 1) {
+              ?>
+                <button type="button" class="clear-button-style" data-toggle="tooltip" data-html="true" data-placement="top" title="<small><?php echo $knownDescription[$data]; ?></small>"><i class="devicon-<?php echo $data; ?> my-icon-single"></i></button>
+              <?php } } ?>
+            </div>
+          </div>
+
+
+          <!-- Section 2 - Learning at the moment -->
+          <div class="container mb-5 pt-5">
+            <h4 class="my-text-title mb-0">Currently Learning</h4>
+            <p class="my-text-title small"><i>Technologies that I am currently studying and experimenting with <small>(Click to see my current percentage progress)</small></i></p>
+
+            <!-- Hr -->
+            <hr class="my-hr-style mb-3"/>
+
+            <!-- Data list -->
+            <div class="my-bg-secondary">
+              <?php
+                foreach($known as $data => $value) {
+                  if ($value == 2) {
+              ?>
+                <button type="button" class="clear-button-style" data-toggle="tooltip" data-placement="top" title="<?php echo $knownDescription[$data]; ?>"><i class="devicon-<?php echo $data; ?> my-icon-single"></i></button>
+              <?php } } ?>
+            </div>
+
+          </div>
+
+          <!-- Section 3 - Skills -->
+          <div class="container mb-5 pt-5">
+            <h4 class="my-text-title mb-0">Personal Skills</h4>
+            <p class="my-text-title small"><i>Soft skills, communication and other abilities <small>(Click to see more information)</small></i></p>
+
+            <!-- Hr -->
+            <hr class="my-hr-style mb-3"/>
+
+            <!-- Data list -->
+            <div class="my-bg-secondary">
+
+              <!-- Dropdowns -->
+              <div id="accordion">
+
+                <?php
+                  $count = 1;
+                  foreach($skills as $data => $value) {
+                    $id = "heading".$count;
+                    $collapse = "collapse".$count;
+                ?>
+                  <div class="card my-bg-primary">
+                    <div class="card-header" id="<?php echo $id; ?>">
+                      <button class="clear-button-style w-100" data-toggle="collapse" data-target="<?php echo '#'.$collapse; ?>" aria-expanded="false" aria-controls="<?php echo $collapse; ?>">
+                        <h5 class="my-text-subtext mb-0 text-left">
+                            <?php echo $data; ?>
+                          <i class="fa fa-angle-down float-right"></i>
+                        </h5>
+                      </button>
+                    </div>
+
+                    <div id="<?php echo $collapse; ?>" class="collapse" aria-labelledby="<?php echo $id; ?>" data-parent="#accordion">
+                      <div class="card-body my-text-subtext">
+                        <small>
+                          <?php echo $value; ?>
+                        </small>
+                      </div>
+                    </div>
+                  </div>
+                <?php
+                  ++$count;
+                  }
+                ?>
+
+
+              </div>
+            </div>
+          </div>
+
+
+        <!-- Made by footer, please do not delete or change, thanks -->
+        <footer class="pt-5 pl-4 pr-4" style="opacity: 0.6">
+          <nav class="nav">
+            <p class="my-text-subtext">
+              <small>Designed and built by <a target="_blank" href="https://github.com/LassSandro">Sandro Maglione</a><br />Support the developer on socials<br />Currently v1.0.0 2018 Code licensed MIT</small>
+            </p>
+          </nav>
+        </footer>
+
+
+       <!-- Close main content section -->
+      </div>
+
+    <!-- Close container -->
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+    <!-- Enable tooltip (from popper.js) -->
+    <script>
+      $( document ).ready(function() {
+        $(function () {
+          $('[data-toggle="tooltip"]').tooltip()
+        })
+      });
+    </script>
+
+  </body>
+</html>
